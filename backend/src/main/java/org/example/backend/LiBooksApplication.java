9571aa94de980b87bd07a6a7d1f1a9a84dad6554
mahfouz72 <mohamadmahfouz39@gmail.com>
2024-10-24 14:53:47 +0300

    initial commit

---
+++ backend/src/main/java/org/example/backend/LiBooksApplication.java
@@ -0,0 +1,13 @@
+package org.example.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiBooksApplication.class, args);
+    }
+
+}
