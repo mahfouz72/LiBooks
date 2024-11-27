09aabd5eed52712fd99f3e5414ad0175385360f7
Youssif Khaled <youssif13abdalaziz@gmail.com>
2024-11-27 20:37:47 +0200

    [LIB-70] Add BookRepository interface for Book entity persistence

---
+++ backend/src/main/java/org/example/backend/repositories/BookRepository.java
@@ -0,0 +1,10 @@
+package org.example.backend.repositories;
+
+import org.example.backend.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+    // Query methods to be added here
+}
