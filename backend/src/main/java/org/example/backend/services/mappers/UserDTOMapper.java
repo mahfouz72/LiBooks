4f3ccd59ae45b0883aed1bf2579b76a71c8d0a58
mahfouz72 <mohamadmahfouz39@gmail.com>
2024-11-30 19:15:41 +0200

    [LIB-56] Create user databases, repositories and service

---
+++ backend/src/main/java/org/example/backend/services/mappers/UserDTOMapper.java
@@ -0,0 +1,22 @@
+package org.example.backend.services.mappers;
+
+
+import org.example.backend.models.dtos.UserDTO;
+import org.example.backend.models.entites.User;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+@Service
+public class UserDTOMapper implements Function<User, UserDTO> {
+
+    @Override
+    public UserDTO apply(User user) {
+        return new UserDTO(user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getDateOfBirth(),
+                user.getDateCreated()
+        );
+    }
+}
