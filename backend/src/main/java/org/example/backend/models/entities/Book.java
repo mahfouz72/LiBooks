db218637ac13c4fd26e6136f9649e26abbd4963b
Youssif Khaled <youssif13abdalaziz@gmail.com>
2024-12-01 18:56:15 +0200

    [refactor] Update Book entity column names to follow snake_case convention

--- backend/src/main/java/org/example/backend/models/entities/Book.java
+++ backend/src/main/java/org/example/backend/models/entities/Book.java
@@ -14,7 +14,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "BOOK")
+@Table(name = "book")
 public class Book {
     private static final int MAX_BOOK_TITLE_LENGTH = 200;
     private static final int MAX_ISBN_LENGTH = 10;
@@ -25,37 +25,33 @@ public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "BOOK_ID")
     private Integer bookId;
 
-    @Column(name = "BOOK_TITLE", nullable = false, length = MAX_BOOK_TITLE_LENGTH)
+    @Column(nullable = false, length = MAX_BOOK_TITLE_LENGTH)
     private String bookTitle;
 
-    @Column(name = "ISBN", length = MAX_ISBN_LENGTH, unique = true)
+    @Column(length = MAX_ISBN_LENGTH, unique = true)
     private String isbn;
 
-    @Column(name = "RATINGS_COUNT")
     private Integer ratingsCount;
 
-    @Column(name = "RATING", precision = 2, scale = 1)
+    @Column(precision = 2, scale = 1)
     private BigDecimal rating;
 
-    @Column(name = "SUMMARY", length = MAX_SUMMARY_LENGTH)
+    @Column(length = MAX_SUMMARY_LENGTH)
     private String summary;
 
-    @Column(name = "BOOK_COVER")
     private byte[] bookCover;
 
-    @Column(name = "LANGUAGE_OF_ORIGIN", length = MAX_LANGUAGE_OF_ORIGIN_LENGTH)
+    @Column(length = MAX_LANGUAGE_OF_ORIGIN_LENGTH)
     private String languageOfOrigin;
 
-    @Column(name = "PUBLICATION_DATE")
     private Date publicationDate;
 
-    @Column(name = "PUBLISHER", length = MAX_PUBLISHER_LENGTH)
+    @Column(length = MAX_PUBLISHER_LENGTH)
     private String publisher;
 
-    @Column(name = "GENRE", length = MAX_GENRE_LENGTH)
+    @Column(length = MAX_GENRE_LENGTH)
     private String genre;
 
 }
