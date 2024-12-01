d3f9a6c84773c0e31426ba778aebf18580dd9eda
Ab00dMD <abdalrhmanmohamed94@gmail.com>
2024-12-01 22:13:28 +0200

    [LIB-60] Add validation to login page

--- frontend/src/login/Login.jsx
+++ frontend/src/login/Login.jsx
@@ -1,25 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import './Login.css'
-import { LuUserCircle2 } from "react-icons/lu";
-import { MdLock } from "react-icons/md";
-
 
 function LoginForm(){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [usernameError, setUsernameError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+
+    const handleSubmit = () => {
+        setUsernameError("");
+        setPasswordError("");
+
+        if(username == ''){
+            setUsernameError("Username is required!");
+            return
+        }
+
+        if(username.length < 2){
+            setUsernameError("The password must be at least 3 characters");
+            return
+        }
+
+        if(password == ''){
+            setPasswordError("Password is required!");
+            return
+        }
+
+        if(password.length < 7){
+            setPasswordError("The password must be at least 8 characters");
+            return
+        }
+
+    };
+
+    
     return(
         <div className="wrapper">
             <form action="">
                 <h1>Login</h1>
+
                 <div className="input-box">
-                    <input type="text" placeholder="username" required/>
+                    <input type="text" 
+                        placeholder="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <p className="error">{usernameError}</p>
                 </div>
+
                 <div className="input-box">
-                    <input type="password" placeholder="password" required/>
+                    <input type="password" 
+                        placeholder="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <p className="error">{passwordError}</p>
                 </div>
+
                 <div className="rememberme-forget">
                     <label htmlFor=""><input type="checkbox"/>Remember me</label>
                     <a href="#">Forget password</a>
                 </div>
-                <button className="login-btn" type="submit">Log in</button>
+                <button className="login-btn" type="button" onClick={handleSubmit}>Log in</button>
                 <button className="google-btn"><span>Continue with Google</span></button>
                 <div className="register">
                     <p>Don't have an account? <a href="#">Register</a></p>
