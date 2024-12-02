22684e05b6f2dd39b998f6ddb4a16cd4e70ad8e1
esmailMahmouds <120949930+esmailMahmouds@users.noreply.github.com>
2024-12-03 01:20:18 +0200

    [LIB-71] Add Blob image in card

--- frontend/src/BookBrowsing/BookCard.jsx
+++ frontend/src/BookBrowsing/BookCard.jsx
@@ -5,49 +5,48 @@ import {
     CardMedia,
     Rating,
     Typography,
-} from "@mui/material";
-
-function BookCard({ bookCover, bookTitle, author, rating }) {
+  } from "@mui/material";
+  
+  function BookCard({ bookCover, bookTitle, rating }) {
     return (
-        <Card sx={{ width: 150 }}>
-            <CardMedia
-                component="img"
-                image={bookCover}
-                sx={{
-                    width: "100%",
-                    height: "200px",
-                    objectFit: "fill",
-                }}
-                title="book"
-            />
-            <CardContent>
-                <Typography
-                    gutterBottom
-                    variant="subtitle1"
-                    component="div"
-                    color="primary"
-                    fontWeight="bold"
-                    sx={{
-                        whiteSpace: "nowrap",
-                        overflow: "hidden",
-                        textOverflow: "ellipsis",
-                    }}
-                >
-                    {bookTitle}
-                </Typography>
-                <Typography variant="body2" sx={{ color: "black" }}>
-                    by {author}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Rating
-                    name="half-rating-read"
-                    defaultValue={rating}
-                    precision={0.5}
-                    readOnly
-                />
-            </CardActions>
-        </Card>
+      <Card sx={{ position: "relative", width: 150, height: 350 }}>
+        <CardMedia
+          component="img"
+          image={`data:image/jpeg;base64,${bookCover}`}
+          sx={{
+            width: "100%",
+            height: "200px",
+            objectFit: "fill",
+          }}
+          title="book"
+        />
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="subtitle1"
+            component="div"
+            color="primary"
+            fontWeight="bold"
+            sx={{
+              whiteSpace: 'nowrap', overflow: 'hidden',textOverflow: 'ellipsis'
+            }}
+          >
+            {bookTitle}
+          </Typography>
+          <Typography variant="body2" sx={{ color: "black" }}>
+            by author
+          </Typography>
+        </CardContent>
+        <CardActions sx={{position: 'absolute' , bottom: 0}}>
+          <Rating
+            name="half-rating-read"
+            defaultValue={rating}
+            precision={0.5}
+            readOnly
+          />
+        </CardActions>
+      </Card>
     );
-}
-export default BookCard;
+  }
+  export default BookCard;
+  
