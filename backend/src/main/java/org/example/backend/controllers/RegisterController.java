1efc994ba6343d8c93c92bea13a2f2f55d002feb
Mohamed Mahfouz <mohamadmahfouz39@gmail.com>
2024-12-03 00:21:31 +0200

    LIB-73 sign-up-in-using-basic-authentication (#15)

--- backend/src/main/java/org/example/backend/controllers/RegisterController.java
+++ backend/src/main/java/org/example/backend/controllers/RegisterController.java
@@ -2,27 +2,25 @@ package org.example.backend.controllers;
 
 
 import org.example.backend.models.dtos.UserDTO;
-import org.example.backend.models.entities.User;
-import org.example.backend.services.UserService;
+import org.example.backend.models.dtos.UserRegistrationDTO;
+import org.example.backend.services.UserAuthenticationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
 public class RegisterController {
 
-    private UserService userService;
+    private UserAuthenticationService userAuthenticationService;
 
-    public RegisterController(UserService userService) {
-        this.userService = userService;
+    public RegisterController(UserAuthenticationService userAuthenticationService) {
+        this.userAuthenticationService = userAuthenticationService;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<UserDTO> register(@RequestBody User user) {
-        UserDTO userDTO = userService.register(user);
+    public ResponseEntity<UserDTO> register(@RequestBody UserRegistrationDTO userRegistrationDTO) {
+        UserDTO userDTO = userAuthenticationService.register(userRegistrationDTO);
         return ResponseEntity.ok(userDTO);
     }
 }
