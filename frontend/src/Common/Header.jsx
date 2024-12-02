486222ab321ca65e2c45382bd6092f66da554b43
esmailMahmouds <120949930+esmailMahmouds@users.noreply.github.com>
2024-12-02 22:55:11 +0200

    [refactor]: Add Button to Home and books tab

--- frontend/src/Common/Header.jsx
+++ frontend/src/Common/Header.jsx
@@ -1,39 +1,66 @@
-import {AppBar, Avatar, Box, IconButton, Stack, Toolbar, Typography} from "@mui/material";
+import {
+    AppBar,
+    Avatar,
+    Box,
+    Button,
+    IconButton,
+    Stack,
+    Toolbar,
+    Typography,
+} from "@mui/material";
 
-function Header(){
-    return(
-        <Box sx={{ flexGrow: 1  }}>
-            <AppBar position="static" sx={{bgcolor:'#974903'}}>
+function Header() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static" sx={{ bgcolor: "#974903" }}>
                 <Toolbar>
                     <IconButton
                         size="small"
                         edge="start"
                         color="inherit"
                         aria-label="menu"
-                        sx={{ mr: 20}}
+                        sx={{ mr: 20 }}
                     >
-                        <Typography variant="h6" fontWeight={"bold"} component="div" sx={{ flexGrow: 1 }}>
+                        <Typography
+                            variant="h6"
+                            fontWeight={"bold"}
+                            component="div"
+                            sx={{ flexGrow: 1 }}
+                        >
                             LIBOOKS
                         </Typography>
                     </IconButton>
 
-                    <Stack direction="row" spacing={4} sx={{ flexGrow: 1 }} justifyContent={"space-around"}>
-                        <Typography variant="h6" component="div" >
-                            Home
-                        </Typography>
-                        <Typography variant="h6" component="div" >
-                            Books
-                        </Typography>
-                        <Stack direction="row" spacing={2} >
-                            <Avatar>A</Avatar>
-                            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    <Stack
+                        direction="row"
+                        spacing={4}
+                        sx={{ flexGrow: 1 }}
+                        justifyContent={"space-around"}
+                    >
+                        <Button color="inherit">Home</Button>
+                        <Button color="inherit">Books</Button>
+                        <Stack direction="row" spacing={2}>
+                            <Button
+                                color="inherit"
+                                sx={{
+                                    '& .MuiTouchRipple-root': {
+                                        borderRadius: '50%',
+                                    },
+                                    "&:hover": {
+                                        borderRadius: '50%',
+                                    },
+                                }}
+                            >
+                                <Avatar>A</Avatar>
+                            </Button>
+                            <Typography variant="h6" component="div">
                                 Ahmed
                             </Typography>
                         </Stack>
                     </Stack>
                 </Toolbar>
             </AppBar>
-        </Box>
-    )
+        </Box >
+    );
 }
 export default Header;
