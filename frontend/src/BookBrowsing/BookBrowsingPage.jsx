d0c1c195a4fbc045bb9b2f6bcdf29a53fb99bf42
esmailMahmouds <120949930+esmailMahmouds@users.noreply.github.com>
2024-12-03 01:13:18 +0200

    [LIB-71] Add Fetching books from backend

--- frontend/src/BookBrowsing/BookBrowsingPage.jsx
+++ frontend/src/BookBrowsing/BookBrowsingPage.jsx
@@ -1,90 +1,47 @@
-import {Stack} from "@mui/material";
+import { Stack } from "@mui/material";
 import Header from "../Common/Header";
 import BookList from "./BookList";
-import bookCover from "../assests/book cover.jpg";
-import bookCover1 from "../assests/book cover1.jpg";
-import bookCover2 from "../assests/book cover2.jpg";
-const books = [
-    {
-        bookCover: bookCover,
-        bookId: 1,
-        bookTitle: "Book",
-        author: "Agatha",
-        rating: 2.5
-    },
-    {
-        bookCover: bookCover1,
-        BookId: 2,
-        bookTitle: "And Then There Were none",
-        author: "Agatha1",
-        rating: 3
-    },
-    {
-        bookCover: bookCover2,
-        bookId: 3,
-        bookTitle: "Book2",
-        author: "Agatha2",
-        rating: 5
-    },
-    {
-        bookCover: bookCover,
-        bookId: 4,
-        bookTitle: "Book",
-        author: "Agatha",
-        rating: 1
-    },
-    {
-        bookCover: bookCover1,
-        bookId: 5,
-        bookTitle: "Book1",
-        author: "Agatha1",
-        rating: 3
-    },
-    {
-        bookCover: bookCover2,
-        bookId: 6,
-        bookTitle: "Book2",
-        author: "Agatha2",
-        rating: 5
-    },
-    {
-        bookCover: bookCover,
-        bookId: 7,
-        bookTitle: "Book",
-        author: "Agatha",
-        rating: 1
-    },
-    {
-        bookCover: bookCover1,
-        bookId: 8,
-        bookTitle: "Book1",
-        author: "Agatha1",
-        rating: 3
-    },
-    {
-        bookCover: bookCover2,
-        bookId: 9,
-        bookTitle: "Book2",
-        author: "Agatha2",
-        rating: 5
-    },
-    {
-        bookCover: bookCover,
-        bookId: 10,
-        bookTitle: "Book",
-        author: "Agatha",
-        rating: 1
-    }
-]
 
-function BookBrowsingPage(){
-    return(
-         <Stack spacing={4}>
-            <Header/>
-            <Stack direction="row" justifyContent="center" spacing={2}  >
-                <BookList books={books} sx={{width:'80%'}}/>
+import { useEffect, useState } from "react";
+
+function BookBrowsingPage() {
+    const [books, setBooks] = useState([]);
+    const [page, setPage] = useState(0);
+    const pageSize = 8;
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const response = await fetch(
+                `http://localhost:8080/books?page=${page}&size=${pageSize}`
+            );
+            const data = await response.json();
+            console.log(data)
+            setBooks((prev) => [...prev, ...data]);
+        };
+        fetchBooks();
+    }, [page]);
+
+    const handleScroll = (e) => {
+        console.log("log:paged scorlled");
+        const { scrollTop, scrollHeight } = e.target.documentElement;
+        const currentHeight = scrollTop + window.innerHeight;
+        if (currentHeight + 1 >= scrollHeight) {
+            setPage((prev) => prev + 1);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    return (
+        <Stack spacing={4}>
+            <Header />
+            <Stack direction="row" justifyContent="center" spacing={2}>
+                <BookList books={books} sx={{ width: "70%" }} />
             </Stack>
         </Stack>
-    )
+    );
 }
 export default BookBrowsingPage;
