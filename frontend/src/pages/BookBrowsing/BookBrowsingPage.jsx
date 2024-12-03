55183ce52e373972209235307eb352ad8d3a67da
Isamel Mahmoud <120949930+esmailMahmouds@users.noreply.github.com>
2024-12-03 21:15:57 +0200

    Lib 71 book controller and integrate with frontend (#17)
    * [LIB-71] Add Fetching books from backend
    
    * [LIB-71] Add Blob image in card
    
    * [LIB-71] Add key prop in Grid
    
    * [LIB-71] Add BookController
    
    * [LIB-71] Change listing books to be pageable
    
    * [LIB-71] Make bookcover type is BLOB
    
    * [FIX] Edit testListBooks to be pageable
    
    * [FIX] Remove strictMode to fix fetch books
    
    * [FIX] Edit format for checkstyle
    
    * [fix] Edit test of ListBooks
    
    * [refactor] Move BookBrowsing folder to pages folder

---
+++ frontend/src/pages/BookBrowsing/BookBrowsingPage.jsx
@@ -0,0 +1,47 @@
+import { Stack } from "@mui/material";
+import Header from "../../Common/Header";
+import BookList from "./BookList";
+
+import { useEffect, useState } from "react";
+
+function BookBrowsingPage() {
+    const [books, setBooks] = useState([]);
+    const [page, setPage] = useState(0);
+    const pageSize = 8;
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const response = await fetch(
+                `http://localhost:8080/books?page=${page}&size=${pageSize}`
+            );
+            const data = await response.json();
+            console.log(data)
+            setBooks((prev) => [...prev, ...data]);
+        };
+        fetchBooks();
+    }, [page]);
+
+    const handleScroll = (e) => {
+        console.log("log:paged scorlled");
+        const { scrollTop, scrollHeight } = e.target.documentElement;
+        const currentHeight = scrollTop + window.innerHeight;
+        if (currentHeight + 1 >= scrollHeight) {
+            setPage((prev) => prev + 1);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    return (
+        <Stack spacing={4}>
+            <Header />
+            <Stack direction="row" justifyContent="center" spacing={2}>
+                <BookList books={books} sx={{ width: "70%" }} />
+            </Stack>
+        </Stack>
+    );
+}
+export default BookBrowsingPage;
