6325f3c815850360082a13fe6a19b1da4d148660
mahfouz72 <mohamadmahfouz39@gmail.com>
2024-12-04 03:11:55 +0200

    fix checkstyle

--- backend/src/main/java/org/example/backend/services/GmailValidationService.java
+++ backend/src/main/java/org/example/backend/services/GmailValidationService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
 
 @Service
 public class GmailValidationService {
