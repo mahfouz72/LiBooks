dfe6fdf68baa901875ed09001e6f47cd71a4732a
Youssif Khaled <youssif13abdalaziz@gmail.com>
2024-12-16 01:39:58 +0200

    refactor: reorganize routes and add profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google'; // Import the GoogleOAuthProvider
-import Signup from './pages/signup/signup';
+
 import Login from './pages/login/Login';
+import Signup from './pages/signup/signup';
+import Profile from './pages/profile/Profile';
 import GmailSignup from './pages/welcome/welcome'; 
 import BookBrowsingPage from "./pages/BookBrowsing/BookBrowsingPage";
 
@@ -10,17 +12,16 @@ function App() {
   return (
     <GoogleOAuthProvider clientId="441471832178-me4tprachirkf15ra6umrlff54i5utlu.apps.googleusercontent.com"> {}
       <Router>
-        <div>
           <main>
             <Routes>
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<GmailSignup />} />
               <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/register" element={<GmailSignup />} />
               <Route path="/BookBrowsingPage" element={<BookBrowsingPage />} />
             </Routes>
           </main>
-        </div>
       </Router>
     </GoogleOAuthProvider>
   );
