7a89b8b2e67d7ba26c4ccd8d2201a35f06fbfb6d
Isamel Mahmoud <120949930+esmailMahmouds@users.noreply.github.com>
2024-12-18 04:08:05 +0200

    [Fix] Add username to profile (#51)

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -3,15 +3,33 @@ import "./Profile.css";
 import Header from "../../Components/Header/Header";
 import BookShelves from "../../Components/BookShelves/BookShelves";
 import ProfileAvatar from "../../Components/ProfileAvatar/ProfileAvatar";
+import { useEffect, useState } from "react";
 
 function Profile() {
-
-    const firstName = "Youssif";
-    const lastName = "Khaled";
-    const username = "youssifkhaled";
+    const [username, setUserName] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
     const followersNumber = 2;
     const followingNumber = 1;
 
+    const token = localStorage.getItem("token");
+
+    useEffect(() => {
+        const getUserName = async () => {
+            const response = await fetch(
+                    `http://localhost:8080/username`, {
+                    method: 'GET',
+                    headers: {"Authorization": `Bearer ${token}`},
+                }
+            );
+            const data = await response.text();
+            console.log(data)
+            setUserName(data)
+            setFirstName(data)
+            setLastName(data)
+        };
+        getUserName();
+    }, [username, token]);
     return (
         <div className="profilePage">
             <Header />
@@ -20,7 +38,7 @@ function Profile() {
                     {/* Avatar, First Name, Last Name, username, followers and following */}
                     <ProfileAvatar firstName={firstName} lastName={lastName} size="large" />
                     <div className="profileInfo">
-                        <h1>{firstName} {lastName}</h1>
+                        <h1>{firstName}</h1>
                         <p>@{username}</p>
                         <div className="follows">
                             <button className="followsButton">
