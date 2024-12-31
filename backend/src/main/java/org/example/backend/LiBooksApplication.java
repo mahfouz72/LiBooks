0125e710a02ab953249aab45a36672260e66c049
Youssif Khaled <youssif13abdalaziz@gmail.com>
2024-12-31 03:50:19 +0200

    feat - add shutdown hook to execute recommendation model script on application exit

--- backend/src/main/java/org/example/backend/LiBooksApplication.java
+++ backend/src/main/java/org/example/backend/LiBooksApplication.java
@@ -1,5 +1,7 @@
 package org.example.backend;
 
+import java.nio.file.Paths;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +10,14 @@ public class LiBooksApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LiBooksApplication.class, args);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                System.out.println("Recommendations computation...");
+                Runtime.getRuntime().exec("python " + Paths.get("").toAbsolutePath().toString() + "\\recommendation-system\\RecommendationModel.py");
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }));
     }
 }
